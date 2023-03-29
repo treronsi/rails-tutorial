9b397a9540f3310310338cea97b00e672de44b4d
treronsi <treron.siebol@gmail.com>
2023-03-29 23:37:12 +0900

    todoapp

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,10 +12,25 @@ class TasksController < ApplicationController
 	 	@task = Task.create(task_params)
 	 	redirect_to tasks_path
 	end
+	
+	def edit
+		@task = Task.find(params[:id])
+	end
+	
+	def update
+		@task = Task.find(params[:id])
+		@task.update(task_params)
+		redirect_to tasks_path
+	end
+	
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy
+		redirect_to tasks_path
+	end	
 	 
 	private
 		def task_params
 			params.require(:task).permit(:title)
-	end
+		end
 end
- 	
