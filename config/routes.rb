886b13bb35ecab4c8a2340dd0af8c82f2a42bc7e
treronsi <treron.siebol@gmail.com>
2023-03-29 23:18:23 +0900

    todoapp

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/info'
-  root to: redirect('/ideas')
+  root to: redirect('/tasks')
   resources :ideas
+  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_145342) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_135738) do
   create_table "ideas", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
