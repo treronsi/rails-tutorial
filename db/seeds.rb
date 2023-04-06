5be47d64a77f04546a01090a2b7a5f449c12d681
treronsi <treron.siebol@gmail.com>
2023-04-06 22:26:40 +0900

    kakeibo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create(inout: 1, category: "income", year: 2023,month: 4,amount: 30)
+Book.create(inout: 2, category: "rent", year: 2023,month: 4,amount: 5)
+Book.create(inout: 2, category: "food", year: 2023,month: 4,amount: 2)
+Book.create(inout: 2, category: "utility", year: 2023,month: 4,amount: 2)
+Book.create(inout: 2, category: "allowance", year: 2023,month: 4,amount: 3)
+
