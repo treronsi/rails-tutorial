1cc79516a194094dc5e62c4e2d14e18cf8b78b49
treronsi <treron.siebol@gmail.com>
2023-04-15 22:59:33 +0900

    kakeibo

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/info'
-  get "/books" ,to: "books#index"
-  post"/books", to: "books#create"
-  	get "/books/new" , to: "books#new", as: "new_book"
-  	get "/books/:id/edit" , to: "books#edit", as: "edit_book"
-  	get "/books/:id" , to: "books#show", as: "book"
-  patch"/books/:id" , to: "books#update"
-   delete 'books/:id' => 'books#destroy'
+  Rails.application.routes.draw do
+  	resources :books
+  end
   root to: redirect('/tasks')
   resources :ideas
   resources :tasks
